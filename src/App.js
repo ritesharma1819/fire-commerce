51c5f97445d4494ad2ee9df9a0568f7ffe2ea8d1
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-17 23:34:09 +0530

    added bootstrap to with our code and change font style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>happy shoppy </h1>
+      <button className='btn btn-primary'>click me</button>
      
     </div>
   );
