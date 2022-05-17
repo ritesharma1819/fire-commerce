d1f5fe464680b2ac47ee9f695bf5cd1924e8b8c5
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-18 00:16:48 +0530

    added navigation bar from bootstrap

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">Happy Shoppy </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">user</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Orders</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">LogOut</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Cart</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 }
 
