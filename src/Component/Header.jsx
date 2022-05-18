20f86b9850e00e27c70de793102f79705f02a12f
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-18 14:28:33 +0530

    changed title name

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <div>
       <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Happy Shoppy </Link>
+          <Link className="navbar-brand" to="/">Fire Commerce </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"> <AiOutlineBars size={25} color='white'/></span>
           </button>
