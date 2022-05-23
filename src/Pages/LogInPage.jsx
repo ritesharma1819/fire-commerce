758131a859081a11af152e16b7a16005246e0f29
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-23 22:57:08 +0530

    added useform hook for input handle

--- src/Pages/LogInPage.jsx
+++ src/Pages/LogInPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function LogInPage() {
+  const {register, handleSubmit}= useForm()
+
+  const loginSubmit=(data)=>{
+    console.log(data)
+  }
   return (
     <div className="login_parent">
       <div className="row">
@@ -15,15 +21,17 @@ function LogInPage() {
           ></lottie-player>
         </div>
         <div className="col-md-4 ">
+        <form onSubmit={handleSubmit(loginSubmit)}>
           <div className="login_fields">
             <div className="login_input">
               <h2>Login </h2>
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <button>Login</button>
+              <input type="email" placeholder="Email" {...register('email')} />
+              <input type="password" placeholder="Password" {...register('password')}/>
+              <button type="submit">Login</button>
               <Link to="/register">Click Here For Register</Link>
             </div>
           </div>
+      </form>
         </div>
       </div>
     </div>
--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function RegisterPage() {
+
+  const {register, handleSubmit}= useForm()
+
+  const registerSubmit=(data)=>{
+    console.log(data)
+  }
   return (
     <div className="register_parent">
       <div className="row">
@@ -15,16 +22,18 @@ function RegisterPage() {
           ></lottie-player>
         </div>
         <div className="col-md-4 ">
+          <form onSubmit={handleSubmit(registerSubmit)}>
           <div className="register_fields">
             <div className="register_input">
               <h2>Register </h2>
-              <input type="text" placeholder="Username" />
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <button>Register</button>
+              <input type="text" placeholder="Username" {...register("username")} />
+              <input type="email" placeholder="Email"  {...register("email")}/>
+              <input type="password" placeholder="Password" {...register("password")} />
+              <button type="submit">Register</button>
               <Link to="/login">Click Here For Login</Link>
             </div>
           </div>
+          </form>
         </div>
       </div>
     </div>
