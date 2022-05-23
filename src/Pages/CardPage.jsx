22ac9866a661011854c9e9d488286a91e935c6db
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-05-23 17:57:51 +0530

    added feature for total amount of items

--- src/Pages/CardPage.jsx
+++ src/Pages/CardPage.jsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import Layout from "../component/Layout";
 import { useDispatch, useSelector } from "react-redux";
 
 const CardPage = () => {
   const { cartItem } = useSelector((state) => state.CartReducer);
+  const [totalAmount, setTotalAmount] = useState(0);
   const dispatch = useDispatch();
 
   const deletefromcart = (item) => {
     dispatch({ type: "DELETE_FROM_CART", payload: item });
   };
+
+  useEffect(() => {
+    let temp = 0;
+    cartItem.forEach((cartItem) => {
+      temp = temp + cartItem.price;
+    });
+    setTotalAmount(temp);
+  }, [cartItem]);
 
   useEffect(() => {
     localStorage.setItem("cartItem", JSON.stringify(cartItem));
@@ -47,6 +56,14 @@ const CardPage = () => {
           })}
         </tbody>
       </table>
+      <div className="d-flex justify-content-end">
+        <h1 className="cartPage_totalamount">
+          Total Amount = {totalAmount} Rs/-
+        </h1>
+      </div>
+      <div className="d-flex justify-content-end mt-2">
+        <button>Place order</button>
+      </div>
     </Layout>
   );
 };
